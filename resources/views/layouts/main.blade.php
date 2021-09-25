cb80e4474729174ab42d38a2c84ab7fc6432454b
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-25 10:52:12 +0700

    membuat validasi, jika salah email/password > 3

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -112,10 +112,16 @@
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
                             </div>
                         </li>
 
@@ -132,7 +138,7 @@
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
                     </div>
                     <div class="row">
-                        @yield('content')
+                        {{-- @yield('content') --}}
                     </div>
                 </div>
                 <!-- /.container-fluid -->
